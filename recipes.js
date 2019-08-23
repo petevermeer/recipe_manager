6b7bc353f984fb10018f4fffec1c310182c42964
Pete Vermeer <pete_1337@outlook.com>
2019-08-22 20:00:17 -0400

    moved recipes out of index.html
    is this a thing that people do? Like use a .js file as a db?

---
+++ recipes.js
@@ -0,0 +1,277 @@
+var menu = {
+	"hard_boiled_eggs":{
+		"meal":"breakfast",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["eggs","large",4,"dairy"]
+			],
+			"directions":""
+		}
+	},
+	"pancakes":{
+		"meal":"breakfast",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["pancake mix","lbs",1,"dry goods"],
+				["butter","tbsp",2, "dairy"],
+				["syrup","tbsp",2,"dry goods"]
+			],
+			"directions":""
+		}
+	},
+	"yogurt_and_granola":{
+		"meal":"breakfast",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["yogurt","cup",4,"dairy"],
+				["granola","cup",2,"dry goods"],
+				["fresh fruit","cup",2,"produce"]
+			],
+			"directions":""
+		}
+	},
+	"oatmeal":{
+		"meal":"breakfast",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["oatmeal","cup",2,"dry goods"],
+			],
+			"directions":""
+		}
+	},
+	"cereal":{
+		"meal":"breakfast",
+		"selected":false,
+		"staple":true,
+		"serves":1,
+		"recipe":{
+			"ingredients":[
+				["milk","cup",0.5,"dairy"],
+				["cereal","cup",1,"dry goods"]
+			],
+			"directions":""
+		}
+	},
+	"peanut_butter_and_jelly":{
+		"meal":"lunch",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["bread","slice",8,"bakery"],
+				["fruit jelly","tbsp",4,"dry goods"],
+				["peanut butter","tbsp",8,"dry goods"]
+			],
+			"directions":""
+		}
+	},
+	"egg_salad":{
+		"meal":"lunch",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["eggs","large",8,"dairy"],
+				["mayonnaise","tsp",4,"dry goods"],
+				["mustard","tbsp",4,"dry goods"],
+				["bread","slice",8,"bakery"]
+			],
+			"directions":""
+		}
+	},
+	"soup":{
+		"meal":"lunch",
+		"selected":false,
+		"staple":true,
+		"serves":2,
+		"recipe":{
+			"ingredients":[
+				["soup","can",1,"dry goods"]
+			],
+			"directions":""
+		}
+	},
+	"grilled_cheese":{
+		"meal":"lunch",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["bread","slice",8,"bakery"],
+				["cheese","slice",8,"dairy"],
+				["butter","tbsp",8,"dairy"]
+			],
+			"directions":""
+		}
+	},
+	"sausage_frittata":{
+		"meal":"dinner",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["breakfast sausage","lbs",1,"meat"],
+				["spinach","handful",1,"produce"],
+				["onion","small",1,"produce"],
+				["potato","small",1,"produce"],
+				["shredded parmesan cheese","handful",1,"dairy"],
+				["eggs","large",8,"dairy"],
+				["butter","tbsp",1,"dairy"]
+			],
+			"directions":"fry sausage in a non-stick skillet over mid-high heat until it is no longer pink. add in veggies and cheese and fry until onions are soft. pour egg mix over meat and veggies, cover, and let cook for 5 minutes. un cover and broil for another 4 minutes. serve with fruit salad or a spring mix."
+		}
+	},
+	"apple_slices":{
+		"meal":"snack",
+		"selected":false,
+		"staple":true,
+		"serves":2,
+		"recipe":{
+			"ingredients":[
+				["eating apple","medium",1,"produce"],
+			],
+			"directions":""
+		}
+	},
+	"cilantro_chicken_and_tomato_avocado_salsa":{
+		"meal":"dinner",
+		"selected":false,
+		"staple":false,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["skinless chicken thighs","lbs",2,"meat"],
+				["garlic","cloves",4,"produce"],
+				["cilantro","bunch",1,"produce"],
+				["olive oil","tbsp",2,"dry goods"],
+				["cherry tomatos","pint",1,"produce"],
+				["avocados","whole",2,"produce"],
+				["red onion","whole",1,"produce"],
+				["lime","whole",1,"produce"]
+			],
+			"directions":"half the cilantro, oil, and lime juice into two bowls. Put the chicken in one with the garlic to marinate. Dice the onion, tomatos and avocados and put in the other bowl. Grill the chicken yo. Put it all on a plate and consume."
+		}
+	},
+	"grilled_peanut_butter_banana_sandwich":{
+		"meal":"lunch",
+		"selected":false,
+		"staple":false,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["peanut butter","tbsp",8,"dry goods"],
+				["butter","tbsp",4,"dry goods"],						
+				["bananas","whole",4,"produce"],
+				["bread","slice",8,"bakery"],						
+			],
+			"directions":"it's just like a grilled cheese, but you gotta put sliced bananas and PB in the bread instead."
+		}
+	},
+	"baked_chicken":{
+		"meal":"dinner",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["skinless chicken breasts","lbs",2,"meat"],
+				["olive oil","tbsp",2,"dry goods"]
+			],
+			"directions":"Bake at 350 until internal temp is 165. Season to taste."
+		}
+	},
+	"berries":{
+		"meal":"snack",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["berries","pint",1,"produce"]
+			],
+			"directions":"Duh."
+		}
+	},
+	"crackers_and_cheese":{
+		"meal":"snack",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["crackers","package",1,"dry goods"],
+				["cheese","lbs",0.5,"dairy"]
+			],
+			"directions":"You can just use old french bread to make croutons instead if you want."
+		}
+	},
+	"mac_and_cheese":{
+		"meal":"dinner",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["bluebox","package",1,"dry goods"],
+				["butter","tbsp",2,"dairy"],
+				["milk","tbsp",2,"dairy"]
+			],
+			"directions":"see the box for details. You can get away with a little less butter if you need to. You can also add a can of tuna or peas to spice it up."
+		}
+	},
+	"spaghetti_and_meat_sauce":{
+		"meal":"dinner",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["ground beef","lbs",1,"meat"],
+				["noodles","cups",2,"dry goods"],
+				["pasta sauce","bottle",1,"dry goods"]
+			],
+			"directions":"Boil the pasta and strain. Brown beef. Mix sauce and beef and noodles and cook to combine."
+		}
+	},
+	"toast":{
+		"meal":"snack",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["bread","slice",4,"bakery"],
+				["butter","tbsp",1,"dairy"]
+			],
+			"directions":"toast that breadzo. throw some pb or jam on that to really janx it up."
+		}
+	},
+	"goldfish":{
+		"meal":"snack",
+		"selected":false,
+		"staple":true,
+		"serves":4,
+		"recipe":{
+			"ingredients":[
+				["goldfish crackers","handfuls",4,"dry goods"]
+			],
+			"directions":"You can do it."
+		}
+	}	
+};
